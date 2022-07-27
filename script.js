e66192c8fcdc9d72b12672c024cfa3c624d2cadf
Ar-Akhil <ar.akhil1511@gmail.com>
2022-07-27 23:32:53 +0530

    Working basic calculator

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const numbers= document.querySelectorAll('.number');
 const operators= document.querySelectorAll('.operator');
 const buttomAnswer = document.querySelector('.answer_btm');
 const topAnswer = document.querySelector('.answer_top');
+const equal = document.querySelector('.equal');
+let calcvalue = null;
+let number = 0;
+let firstnumber = 0;
+let secondNumber = 0;
 
 numbers.forEach(number => 
   number.addEventListener('click', (e)=>{
-  console.log('number clicked');
   if(buttomAnswer.textContent === '0'){
     buttomAnswer.textContent = '';
     buttomAnswer.textContent = e.target.textContent;
@@ -15,6 +19,35 @@ numbers.forEach(number =>
   }))
 
 operators.forEach(operator => 
-  operator.addEventListener('click', (e)=>
-  console.log('operator clicked'))
-)
+  operator.addEventListener('click', (e)=>{
+    sign = e.target.textContent;
+    if(firstnumber == 0){
+      firstnumber = buttomAnswer.textContent;
+      console.log(firstnumber)
+      buttomAnswer.textContent = '';
+      return firstnumber;
+    }else{
+      secondNumber = buttomAnswer.textContent;
+      console.log(secondNumber)
+      return secondNumber;
+    }
+  }))
+
+  equal.addEventListener('click',()=>{
+    secondNumber = buttomAnswer.textContent;
+    switch(sign){
+    case '+':
+      result = (Number(firstnumber) + Number(secondNumber));
+      break;
+    case '-':
+      result = (Number(firstnumber) - Number(secondNumber));
+      break;
+    case 'x':
+      result = (Number(firstnumber) * Number(secondNumber));
+      break;
+    case '/':
+      result = (Number(firstnumber) / Number(secondNumber));
+      break;
+    }
+    buttomAnswer.textContent = result;
+  })
